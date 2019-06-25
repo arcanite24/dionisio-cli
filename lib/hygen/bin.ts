c0fbf7b457fa05cc202fc74cff79281e56500f1f
Brandon G. Neri <arcaniteamp@gmail.com>
2019-06-25 17:45:46 -0500

    added lib/ to whitelist

--- lib/hygen/bin.ts
+++ lib/hygen/bin.ts
@@ -1,7 +1,7 @@
 import { runner } from './index';
-const Logger = require('./logger')
-const path = require('path')
-const defaultTemplates = path.join(__dirname, '../src/templates')
+const Logger = require('./logger');
+const path = require('path');
+const defaultTemplates = path.join(__dirname, '../src/templates');
 
 const invoke = (params: string[]) => runner(params, {
   templates: defaultTemplates,
@@ -9,8 +9,8 @@ const invoke = (params: string[]) => runner(params, {
   logger: new Logger(console.log.bind(console)),
   debug: !!process.env.DEBUG,
   exec: (action: any, body: any) => {
-    const opts = body && body.length > 0 ? { input: body } : {}
-    return require('execa').shell(action, opts)
+    const opts = body && body.length > 0 ? { input: body } : {};
+    return require('execa').shell(action, opts);
   },
   createPrompter: () => require('enquirer')
 });
